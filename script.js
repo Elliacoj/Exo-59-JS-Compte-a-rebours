e40ce59165967df4972791df769beb7654c7999b
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-11-30 17:11:39 +0100

    Modif

---
+++ script.js
@@ -0,0 +1,11 @@
+let container = document.getElementById("container");
+let  compte = setInterval(comptearebours, 1000);
+let nombre = prompt("Ecrivez 60");
+
+function comptearebours() {
+    container.innerHTML = nombre;
+    nombre--;
+    if (nombre < 0) {
+        clearInterval(compte);
+    }
+}
